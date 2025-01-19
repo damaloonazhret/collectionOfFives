f8342de5f42b99460280257b95c13c2f488f1c7c
damaloonazhret <poiprizamoda@gmail.com>
2025-01-19 12:32:28 +0300

    feat: gh-pages

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-// Инициализация начального значения из localStorage
 let score = localStorage.getItem('score') ? parseInt(localStorage.getItem('score')) : 0;
 document.getElementById('score').textContent = score;
 
-// Звук для кнопки
 const audio = new Audio('https://www.fesliyanstudios.com/play-mp3/7012');
 
-// Функция добавления пятерки
 function addFive() {
-    // Увеличиваем счет
     score++;
     localStorage.setItem('score', score);
     document.getElementById('score').textContent = score;
 
-    // Анимация падающей пятерки
     const jar = document.querySelector('.jar');
     const fallingFive = document.createElement('div');
     fallingFive.classList.add('falling-5');
@@ -20,12 +15,10 @@ function addFive() {
     fallingFive.style.left = Math.random() * 150 + 'px'; // Рандомное положение
     jar.appendChild(fallingFive);
 
-    // Убираем пятерку после анимации
     setTimeout(() => {
         fallingFive.remove();
     }, 2000);
 
-    // Воспроизводим звук
     audio.currentTime = 0;
     audio.play();
 }
